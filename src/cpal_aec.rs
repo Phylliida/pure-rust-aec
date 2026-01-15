46c545cbe1699d49a589d175063c96692676930d
Phylliida <phylliida.dev@gmail.com>
2026-01-14 16:27:29 -0800

    Fixing alignment

--- src/cpal_aec.rs
+++ src/cpal_aec.rs
@@ -2255,7 +2255,7 @@ impl AecStream {
             println!("Making aec with {} inputs and {} outputs", self.input_channels, self.output_channels);
             Some(VoipAec3::builder(self.aec_config.target_sample_rate as i32, self.output_channels, self.input_channels)
             .initial_delay_ms((self.aec_config.frame_size/3) as i32)
-            .enable_high_pass(true)
+            .enable_high_pass(false)
             .build()
             .expect("failed to create AEC pipeline"))
         } else {
